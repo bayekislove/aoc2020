83acd08b51e5782201abefbb99e43b465b809a70
bayekislove <kacperb2000@o2.pl>
2021-04-06 00:03:10 +0200

    added task3

---
+++ task3/task3b.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+enum field
+{
+    open_square,
+    tree
+};
+
+class Board
+{
+    std::vector<field> fields;
+
+    const int width;
+    int height;
+
+    public:
+        void saveLine( const std::string& line )
+        {
+            for( const char& c : line  )
+            {
+                fields.push_back( c == '#' ? tree : open_square );
+            }
+                
+            height++;
+        }
+
+        Board( int line_size ) : width(line_size) 
+        { 
+            height = 0;
+            fields = std::vector<field>();
+        }
+
+        int treesEncountered( int rowDiff, int colDiff )
+        {
+            int trees(0);
+            int last_column(0);
+            for( int i(colDiff); i < height; )
+            {
+                last_column = (last_column + rowDiff) % width;
+                trees += fields[ i * width + last_column ];
+                i += colDiff;
+            }
+            return trees;
+        }
+};
+
+Board get_entries()
+{
+    std::ifstream fromFile("task3.txt");
+
+    std::string fileLine;
+    getline(fromFile, fileLine);
+
+    Board fields( fileLine.length() );
+    fields.saveLine( fileLine );
+
+    while(getline(fromFile, fileLine))
+    { 
+        fields.saveLine( fileLine );
+    };
+
+    return fields;
+}
+
+int main()
+{
+    uint64_t res = 1;
+    res *= get_entries().treesEncountered(3, 1);
+    res *= get_entries().treesEncountered(1, 1);
+    res *= get_entries().treesEncountered(5, 1);
+    res *= get_entries().treesEncountered(7, 1);
+    res *= get_entries().treesEncountered(1, 2);
+
+    printf("%lu \n", res);
+}
